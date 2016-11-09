664706d5419495743c9bc497e5dc7d76c47807a9
Ashur Cabrera <ashur@panic.com>
2016-11-09 13:33:03 -0800

    added JSON wrapper class

---
+++ src/Core/JSON.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * This file is part of Cranberry\Core
+ */
+namespace Cranberry\Core;
+
+class JSON
+{
+	/**
+	 * Perform standard JSON decode, throwing an exception on error
+	 *
+	 * @param	string	$string
+	 * @param	boolean	$assoc
+	 * @param	int		$depth
+	 * @param	int		$options
+	 * @return	array
+	 */
+	static public function decode( $string, $assoc=false, $depth=512, $options=0 )
+	{
+		$result = json_decode( $string, $assoc, $depth, $options );
+
+		$errorCode = json_last_error();
+		$errorMessage = json_last_error_msg();
+
+		if( $errorCode != 0 )
+		{
+			throw new \UnexpectedValueException( $errorMessage, $errorCode );
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Perform standard JSON encode, throwing an exception on error
+	 *
+	 * @param	array	$array
+	 * @param	int		$options
+	 * @param	int		$depth
+	 * @return	string
+	 */
+	static public function encode( array $array, $options=0, $depth=512 )
+	{
+		$result = json_encode( $array, $options, $depth );
+
+		$errorCode = json_last_error();
+		$errorMessage = json_last_error_msg();
+
+		if( $errorCode != 0 )
+		{
+			throw new \UnexpectedValueException( $errorMessage, $errorCode );
+		}
+
+		return $result;
+	}
+}
